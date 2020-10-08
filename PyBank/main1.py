538a8e1fa7c40537a6c0ffece6d56da970f3f25c
Kim <68557743+fineprintkim@users.noreply.github.com>
2020-10-07 20:44:07 -0400

    almost done with pt1

--- PyBank/main1.py
+++ PyBank/main1.py
@@ -11,28 +11,26 @@ with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     if csv.Sniffer().has_header:
         next(csvreader)
-    #print("hi")
-    #csvReader = csv.reader(codecs.open('budget_data.csv', 'rU', 'utf-16'))
-    #header=next(csvreader)
-    #data=[r for r in reader]
-
-#initialize accumulators
+ 
+    #  initialize variables
     number_of_months=0
     totalrevenue=0
-    average=0
     all_differences=[]
     greatest_increase=0
     greatest_decrease=0
     greatestmonth=0
     leastmonth=0
-   
+    month_counter=[]
     
+    first_row = next(csvreader)
+    number_of_months += 1
+    totalrevenue += int(first_row[1])
+    last_profit = int(first_row[1])
+    greatest_increase=int(first_row[1])
+    greatestmonth = first_row[0]
     for row in csvreader:
-    #keep track of the starting values
-        last_profit=int(row[1])
-        max_increase=int(row[1])
-        greatest_increase=int(row[1])
-        greatest_decrease=row[0]
+        
+    
     #find The total number of months included in the dataset
         number_of_months+=1
 
@@ -46,32 +44,39 @@ with open(csvpath) as csvfile:
         all_differences.append(change)
         #reset the last profit for next time
         last_profit = int(row[1])
+        month_counter.append(row[0])
 
     #The greatest increase/decrease in profits (date and amount) over the entire period
          #find the dates too
         if int(row[1]) > greatest_increase:
             #if it is bigger you want it to now equal that value 
-            greatestincrease=int(row[1])
+            greatest_increase=int(row[1])
             #Month associated with it is
-            greatestmonth=int(row[0])
-        if int(row[1]) > greatest_decrease:
+            greatestmonth=row[0]
+        if int(row[1]) < greatest_decrease:
             #if it is bigger you want it to now equal that value 
             greatest_decrease=int(row[1])
             #Month associated with it is
-            leastmonth=int(row[0])
+            leastmonth=row[0]
+        #print(change)
 
        
        
     #The average of the changes in "Profit/Losses" over the entire period
-    average=sum(all_differences)/len(all_differences)
-    max=max(all_differences)
-    min=min(all_differences)
+    average=(sum(all_differences))/len(all_differences)
+    highest=max(all_differences)
+    lowest=min(all_differences)
 
     #print all it out 
     print("Financial Analysis")  
     print("----------------------------")
     print("Total Months:" + str(number_of_months))      
-    print("Total:" + str(totalrevenue))
-    print("Average Change: " + str(average))
-    print("Greatest Increase in Profits:" + greatestmonth + str(greatest_increase))
-    print("Greatest Decrease in Profits:" + leastmonth + str(gratest_decrease))
+    print("Total: $" + str(totalrevenue))
+    print("Average Change: $" + str(average))
+    print("Greatest Increase in Profits: " + greatestmonth + ", $" + str(highest))
+    print("Greatest Decrease in Profits: " + leastmonth + ", $" + str(lowest))
+   
+   
+    #with open(file_to_output, "w") as txt_file:
+        #txt_file.write(output)
+
